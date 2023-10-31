07f71ca215a0ee20fa152cc5f973688dc1a48573
carolinaarantes <2716629@collegelacite.ca>
2023-10-31 11:12:16 -0400

    Changement folder models

--- models/Utilisateur.js
+++ models/Utilisateur.js
@@ -4,19 +4,34 @@ import { DataTypes } from "sequelize";
 //Amener la connexion a la base de donnees
 import database from "../connexion.js"
 
-//Creation du  modele etudiant
+//Creation du  modele utilisateur
 
 export const Utilisateur = database.define(`Utilisateur`,{
-    prenom:{
-        type: DataTypes.STRING, 
-        allowNull:false
+    //PK ici ??????????????????????????????????????????????????????????????
+    utilisateur_id: { 
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
     },
     nom:{
         type: DataTypes.STRING, 
         allowNull:false
     },
+    prenom:{
+        type: DataTypes.STRING, 
+        allowNull:false
+    },
     dateNaissance:{
-        type:DataTypes.DATEONLY
+        type:DataTypes.DATEONLY,
+        allowNull:false
+    },
+    photo:{
+        type: DataTypes.BLOB("long"), 
+        allowNull:true
+    },
+    numeroTelephone:{
+        type: DataTypes.STRING, 
+        allowNull:false
     },
     email:{
         type: DataTypes.STRING, 
